cc0abd3a439a0ad465fa8bd8da8a7114e075f8bc
jhgonzalezuruguay <102374190+jhgonzalezuruguay@users.noreply.github.com>
2025-07-28 10:46:09 -0300

    Create db.py

---
+++ db.py
@@ -0,0 +1,11 @@
+from sqlmodel import create_engine, SQLModel, Session
+import os
+
+sqlite_file = os.getenv("SQLITE_FILE", "db.sqlite3")
+engine = create_engine(f"sqlite:///{sqlite_file}")
+
+def create_db_and_tables():
+    SQLModel.metadata.create_all(engine)
+
+def get_session():
+    return Session(engine)
